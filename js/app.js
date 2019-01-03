56db3b89e1a259655e6cd7de9243e22011951ef3
Nawal00 <nawalgurung80@gmail.com>
2019-01-03 16:00:48 +0000

    grid set up

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const h1 = document.querySelector('h1')
-  h1.textContent = 'Hello World'
+$(() => {
+  const width = 10
+  let playerIndex = 90
+  let direction = 'forward'
+  const $grid = $('.grid')
+
+  //create square grid with divs
+  $grid.attr('data-width', width)
+
+  for(let i = 0; i < width*width; i++) {
+    $grid.append($('<div />'))
+  }
+
+  //create player at the bottom of the grid
+  const $squares = $('.grid div')
+  $squares.eq(playerIndex).addClass('player')
+
+
+
 })
