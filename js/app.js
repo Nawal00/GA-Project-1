ede9b2ea456c63ed67566b8e51ec62fc1592e315
Nawal00 <nawalgurung80@gmail.com>
2019-01-03 16:03:14 +0000

    added function to move player left & right

--- js/app.js
+++ js/app.js
@@ -21,6 +21,26 @@ $(() => {
       .attr('data-direction', direction)
   }
 
+  // move player left and right
+  $(document).on('keydown', e => {
+    // left 37, right 39
+    $squares.eq(playerIndex).removeClass('player')
+
+    switch(e.keyCode) {
+      case 37: if(playerIndex % width > 0){
+        playerIndex--
+        direction = 'backward'
+      }
+        break
+      case 39: if(playerIndex % width < width-1) {
+        playerIndex++
+        direction = 'forward'
+      }
+        break
+    }
+    movePlayer()
+  })
+
 
 
 })
